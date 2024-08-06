319bb21f82d50804de6ce5508429b829b18dac09
Christopher <114704469+Blade-24@users.noreply.github.com>
2024-08-05 22:03:24 -0600

    add menus and animation for them

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Header from "./components/Header";
+import "./App.css";
 
 function App() {
-  return <div>Hola mundo</div>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react";
+import MenuIcon from "../assets/icons/hamburger-menu.png";
+import Logo from "../assets/icons/logo-transparent.png";
+
+function Header() {
+  const [menu, setMenu] = useState(false);
+
+  const handleMenu = () => {
+    setMenu((prevState) => !prevState);
+  };
+
+  return (
+    <header className="container__header">
+      <div className="container__header__primaryBar__content">
+        <img
+          className="container__header__primaryBar__content__menuIcon"
+          src={MenuIcon}
+          alt="Menu"
+          onClick={handleMenu}
+        />
+        <h2 className="container__header__primaryBar__content__textTitle">
+          Fruit <br></br> Burst
+        </h2>
+      </div>
+      <aside
+        className={
+          menu
+            ? "container__header__secondBar"
+            : "container__header__secondBar-close"
+        }
+      >
+        <nav className="container__header__secondBar__content">
+          <img src="" alt="" />
+          <div>
+            <p>as</p>
+          </div>
+          <div>
+            <p>asd</p>
+          </div>
+          <h4></h4>
+        </nav>
+      </aside>
+    </header>
+  );
+}
+
+export default Header;
